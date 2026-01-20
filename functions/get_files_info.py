335b77e027d061b9b9dd15491666d3af93ee4dd1
Yonair <jonercaguiat5@gmail.com>
2026-01-19 17:41:54 -0800

    Added calculator + get file info functions

---
+++ functions/get_files_info.py
@@ -0,0 +1,27 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+    if not valid_target_dir:
+        raise Exception(f'Error: Cannot list "{directory}" as it is outside the permitted working directory.')
+    if not directory:
+        raise Exception(f'Error: "{directory}" is not a directory')
+
+    
+
+    try:
+        target_file = []
+        for items in os.listdir(target_dir):
+            item_path = os.path.join(target_dir, items)
+            is_dir = os.path.isdir(item_path)
+            file_name = os.path.basename(item_path)
+            file_size = os.path.getsize(item_path)
+            target_file.append(f"- {file_name}: file_size={file_size} bytes, is_dir={is_dir}")
+                
+        return "\n".join(target_file)
+
+    except Exception as e:
+        print(f"Error: {e}")
--- main.py
+++ main.py
@@ -5,7 +5,20 @@ from google import genai
 from google.genai import types
 
 
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=messages,
+    )
 
+    if response.usage_metadata is None:
+        raise RuntimeError("Usage metadata is not available.")
+
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+        
+
+    print("Response:", response.text)
 
 def main():
     load_dotenv()
@@ -21,6 +34,7 @@ def main():
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+    
 
     messages = [types.Content(
         role="user",
@@ -28,20 +42,28 @@ def main():
 
     prompt = args.user_prompt
 
-    print("User prompt:", prompt)
-    
-    response = client.models.generate_content(
-        model='gemini-2.5-flash', 
-        contents=messages,
-        )
 
-    
-    
-    if response.usage_metadata is None:
-        raise RuntimeError("Usage metadata is not available.")
-    
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print("User prompt:", prompt)
 
-    print("Response:", response.text)
+    generate_content(client, messages, args.verbose)
+    
+
+
 if __name__ == "__main__":
     main()
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model='gemini-2.5-flash',
+        contents=messages,
+    )
+
+    if response.usage_metadata is None:
+        raise RuntimeError("Usage metadata is not available.")
+
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+        
+
+    print("Response:", response.text)
